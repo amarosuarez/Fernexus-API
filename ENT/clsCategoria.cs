0e5097c889942ae33ad2c896e6607173301536bc
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2025-02-14 12:27:21 +0100

    Capa ENT terminada

---
+++ ENT/clsCategoria.cs
@@ -0,0 +1,25 @@
+﻿namespace ENT
+{
+    public class clsCategoria
+    {
+        #region Propiedades
+        int IdCategoria { get; set; }
+        string Nombre { get; set; }
+        #endregion
+
+        #region Constructores
+        public clsCategoria() { }
+
+        public clsCategoria(int idCategoria)
+        {
+            IdCategoria = idCategoria;
+        }
+
+        public clsCategoria(int idCategoria, string nombre)
+        {
+            this.IdCategoria = idCategoria;
+            this.Nombre = nombre;
+        }
+        #endregion
+    }
+}
---
+++ ENT/clsProovedor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT
+{
+    public class clsProovedor
+    {
+        #region Propiedades
+        int IdProovedor { get; set; }
+        string Nombre { get; set; }
+        string Correo { get; set; }
+        string Telefono { get; set; }
+        string Direccion { get; set; }
+        string Pais { get; set; }
+        #endregion
+
+        #region Constructores
+        public clsProovedor() { }
+
+        public clsProovedor(int IdProovedor)
+        {
+            this.IdProovedor = IdProovedor;
+        }
+
+        public clsProovedor(string Nombre, string Correo, string Telefono, string Direccion, string Pais)
+        {
+            this.Nombre = Nombre;
+            this.Correo = Correo;
+            this.Telefono = Telefono;
+            this.Direccion = Direccion;
+            this.Pais = Pais;
+        }
+        #endregion
+    }
+}
