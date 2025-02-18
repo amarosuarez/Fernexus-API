0a2f3d90800b156dabaa2b0d55764d00d9b4745a
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2025-02-18 14:31:45 +0100

    Update clsListadoProovedoresDAL.cs

--- DAL/clsListadoProovedoresDAL.cs
+++ DAL/clsListadoProovedoresDAL.cs
@@ -74,5 +74,62 @@ namespace DAL
             }
             return listaProovedores;
         }
+
+        public static List<clsProovedor> obtenerListadoProovedoresPorPaisDAL(string pais)
+        {
+            List<clsProovedor> listaProovedores = new List<clsProovedor>();
+
+            clsConexion miConexion = new clsConexion();
+
+            SqlCommand miComando = new SqlCommand();
+
+            SqlDataReader miLector;
+
+            clsProovedor oProovedor;
+
+            try
+            {
+                miComando.CommandText = "EXEC FiltrarProovedoresPorPais @Pais";
+                miComando.Parameters.AddWithValue("@Pais", pais);
+
+                miComando.Connection = miConexion.ObtenerConexion();
+
+                miLector = miComando.ExecuteReader();
+
+                if (miLector.HasRows)
+                {
+                    while (miLector.Read())
+                    {
+                        oProovedor = new clsProovedor();
+
+                        oProovedor.IdProovedor = (int)miLector["IdProveedor"];
+
+                        oProovedor.Nombre = (string)miLector["Nombre"];
+
+                        oProovedor.Correo = (string)miLector["Correo"];
+
+                        oProovedor.Telefono = (string)miLector["Telefono"];
+
+                        oProovedor.Direccion = (string)miLector["Direccion"];
+
+                        oProovedor.Pais = (string)miLector["Pais"];
+
+
+                        listaProovedores.Add(oProovedor);
+                    }
+                }
+                miLector.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                miConexion.Desconectar();
+            }
+
+            return listaProovedores;
+        }
     }
 }
