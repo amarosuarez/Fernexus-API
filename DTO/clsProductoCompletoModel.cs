79a28786644f8c8a6749d651083ac9b054989f3c
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2025-02-25 12:44:12 +0100

    Falta terminar listado Pedidos

--- DTO/clsProductoCompletoModel.cs
+++ DTO/clsProductoCompletoModel.cs
@@ -1,31 +1,37 @@
-﻿namespace DTO
+﻿using ENT;
+
+namespace DTO
 {
     public class clsProductoCompletoModel
     {
         #region Propiedades
         public int idProducto { get; set; }
-        public int idProovedor { get; set; }
+        public clsProveedor proveedor { get; set; }
         public string nombre { get; set; }
         public double precioUd { get; set; }
         public int cantidad { get; set; }
         public double precioTotal { get; set; }
+        public clsCategoria categoria { get; set; }
+
         #endregion
 
         #region Constructores
         public clsProductoCompletoModel() { }
 
-        public clsProductoCompletoModel(int idProducto, int idProovedor)
+        public clsProductoCompletoModel(int idProducto,clsProveedor clsProveedor)
         {
             this.idProducto = idProducto;
-            this.idProovedor = idProovedor;
+            this.proveedor = clsProveedor;
         }
 
-        public clsProductoCompletoModel(int idProducto, int idProovedor, string nombre, double precioUd, int cantidad, double precioTotal) : this(idProducto, idProovedor)
+        public clsProductoCompletoModel(int idProducto, clsProveedor clsProveedor, string nombre, double precioUd, int cantidad, double precioTotal,clsCategoria categoria) : this(idProducto, clsProveedor)
         {
             this.nombre = nombre;
             this.precioUd = precioUd;
             this.cantidad = cantidad;
             this.precioTotal = precioTotal;
+            this.categoria = categoria;
+            this.proveedor = clsProveedor;
         }
 
         #endregion
